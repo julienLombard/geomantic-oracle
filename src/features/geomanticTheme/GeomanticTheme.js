87987d1beb36d2aa5d0f40397bc9b863a408bf19
julienLombard <julienlombard.fr@gmail.com>
2022-03-23 11:14:58 +0100

    adding geomanticTheme.css

--- src/features/geomanticTheme/GeomanticTheme.js
+++ src/features/geomanticTheme/GeomanticTheme.js
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { mountTheme, getGeomanticTheme } from './geomanticThemeSlice';
 import { figureHouseData } from '../../data/figureHouseData';
+import geomanticThemeTemplate from './geomantic_theme_template.png';
+import './geomanticTheme.css';
 
 export const GeomanticTheme = () => {
   const displayTheme = useSelector(getGeomanticTheme);
   const dispatch = useDispatch();
-  console.log(displayTheme);
   const renderFigures =
     displayTheme.mother1.name !== ''
       ? figureHouseData.map((e, i) => {
           return (
-            <div key={i}>
-              <p>{'Maison ' + e.house + ' : ' + displayTheme[e.name].name}</p>
-              <p>{displayTheme[e.name].firstLine === 1 ? ' * ' : '* *'}</p>
-              <p>{displayTheme[e.name].secondLine === 1 ? ' * ' : '* *'}</p>
-              <p>{displayTheme[e.name].thirdLine === 1 ? ' * ' : '* *'}</p>
-              <p>{displayTheme[e.name].fourthLine === 1 ? ' * ' : '* *'}</p>
-              <br />
+            <div key={i} className="figure-div" id={'figure-' + (i + 1)}>
+              <p className="figure-points">
+                {displayTheme[e.name].firstLine === 1 ? ' . ' : '. .'}
+              </p>
+              <p className="figure-points">
+                {displayTheme[e.name].secondLine === 1 ? ' . ' : '. .'}
+              </p>
+              <p className="figure-points">
+                {displayTheme[e.name].thirdLine === 1 ? ' . ' : '. .'}
+              </p>
+              <p className="figure-points">
+                {displayTheme[e.name].fourthLine === 1 ? ' . ' : '. .'}
+              </p>
+              <p className="figure-name">{displayTheme[e.name].name}</p>
             </div>
           );
         })
@@ -26,6 +34,9 @@ export const GeomanticTheme = () => {
     <div>
       <h1>GeomanticTheme</h1>
       <button onClick={() => dispatch(mountTheme())}>mount Theme</button>
+      <div>
+        <img src={geomanticThemeTemplate} alt="geomantic theme template" />
+      </div>
       {renderFigures}
     </div>
   );
