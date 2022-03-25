4c28b21e5e0787aec99e5dc481083652e21e40b0
julienLombard <julienlombard.fr@gmail.com>
2022-03-25 12:18:09 +0100

    adding form  to set mother figures

--- src/features/geomanticTheme/GeomanticTheme.js
+++ src/features/geomanticTheme/GeomanticTheme.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { mountTheme, getGeomanticTheme } from './geomanticThemeSlice';
 import { figureHouseData } from '../../data/figureHouseData';
 import geomanticThemeTemplate from './geomantic_theme_template.png';
+import { isOdd } from './geomanticThemeFunctions';
 import './geomanticTheme.css';
 
 export const GeomanticTheme = () => {
@@ -30,10 +31,96 @@ export const GeomanticTheme = () => {
           );
         })
       : null;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const fourMothers = [];
+    for (let i = 0; i < 16; i++) {
+      if (e.target[i].value !== '') {
+        fourMothers.push(isOdd(e.target[i].value));
+      }
+    }
+
+    if (fourMothers.length === 16) {
+      dispatch(mountTheme(fourMothers));
+    }
+  };
+
   return (
     <div>
       <h1>GeomanticTheme</h1>
-      <button onClick={() => dispatch(mountTheme)}>mount Theme</button>
+      <form className="motherForm" onSubmit={handleSubmit}>
+        <div className="motherForm-div">
+          <label className="motherForm-label">
+            First mother:
+            <input className="motherForm-input" type="number" placeholder="1" />
+            <input className="motherForm-input" type="number" placeholder="2" />
+            <input className="motherForm-input" type="number" placeholder="3" />
+            <input className="motherForm-input" type="number" placeholder="4" />
+          </label>
+        </div>
+
+        <div className="motherForm-div">
+          <label className="motherForm-label">
+            Second mother:
+            <input className="motherForm-input" type="number" placeholder="5" />
+            <input className="motherForm-input" type="number" placeholder="6" />
+            <input className="motherForm-input" type="number" placeholder="7" />
+            <input className="motherForm-input" type="number" placeholder="8" />
+          </label>
+        </div>
+
+        <div className="motherForm-div">
+          <label className="motherForm-label">
+            Third mother:
+            <input className="motherForm-input" type="number" placeholder="9" />
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="10"
+            />
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="11"
+            />
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="12"
+            />
+          </label>
+        </div>
+
+        <div className="motherForm-div">
+          <label className="motherForm-label">
+            Fourth mother:
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="13"
+            />
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="14"
+            />
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="15"
+            />
+            <input
+              className="motherForm-input"
+              type="number"
+              placeholder="16"
+            />
+          </label>
+        </div>
+
+        <button type="submit">mount Theme</button>
+      </form>
       <div>
         <img src={geomanticThemeTemplate} alt="geomantic theme template" />
       </div>
--- src/features/geomanticTheme/geomanticThemeSlice.js
+++ src/features/geomanticTheme/geomanticThemeSlice.js
@@ -8,13 +8,12 @@ import {
   setFigures,
 } from './geomanticThemeFunctions';
 
-const testArr = [1, 1, 2, 2, 1, 2, 1, 2, 2, 2, 1, 1, 2, 1, 2, 2];
-
-export const mountTheme = (linesArr = testArr) => {
+export const mountTheme = (linesArr) => {
   if (linesArr) {
     return {
       type: 'geomanticTheme/mountTheme',
       payload: {
+        value: true,
         mother1: setFigures(linesArr[0], linesArr[1], linesArr[2], linesArr[3]),
         mother2: setFigures(linesArr[4], linesArr[5], linesArr[6], linesArr[7]),
         mother3: setFigures(
@@ -32,7 +31,7 @@ export const mountTheme = (linesArr = testArr) => {
       },
     };
   }
-  return null;
+  return { type: 'geomanticTheme/mountTheme', payload: { value: false } };
 };
 
 const initialState = initialStateData;
@@ -42,125 +41,127 @@ export const geomanticTheme = createSlice({
   initialState,
   reducers: {
     mountTheme(state, { payload }) {
-      state.mother1 = payload.mother1;
-      state.mother2 = payload.mother2;
-      state.mother3 = payload.mother3;
-      state.mother4 = payload.mother4;
+      if (payload.value !== false) {
+        state.mother1 = payload.mother1;
+        state.mother2 = payload.mother2;
+        state.mother3 = payload.mother3;
+        state.mother4 = payload.mother4;
 
-      // daughter
-      state.daughter1 = setFigures(
-        state.mother1.firstLine,
-        state.mother2.firstLine,
-        state.mother3.firstLine,
-        state.mother4.firstLine
-      );
+        // daughter
+        state.daughter1 = setFigures(
+          state.mother1.firstLine,
+          state.mother2.firstLine,
+          state.mother3.firstLine,
+          state.mother4.firstLine
+        );
 
-      state.daughter2 = setFigures(
-        state.mother1.secondLine,
-        state.mother2.secondLine,
-        state.mother3.secondLine,
-        state.mother4.secondLine
-      );
+        state.daughter2 = setFigures(
+          state.mother1.secondLine,
+          state.mother2.secondLine,
+          state.mother3.secondLine,
+          state.mother4.secondLine
+        );
 
-      state.daughter3 = setFigures(
-        state.mother1.thirdLine,
-        state.mother2.thirdLine,
-        state.mother3.thirdLine,
-        state.mother4.thirdLine
-      );
+        state.daughter3 = setFigures(
+          state.mother1.thirdLine,
+          state.mother2.thirdLine,
+          state.mother3.thirdLine,
+          state.mother4.thirdLine
+        );
 
-      state.daughter4 = setFigures(
-        state.mother1.fourthLine,
-        state.mother2.fourthLine,
-        state.mother3.fourthLine,
-        state.mother4.fourthLine
-      );
+        state.daughter4 = setFigures(
+          state.mother1.fourthLine,
+          state.mother2.fourthLine,
+          state.mother3.fourthLine,
+          state.mother4.fourthLine
+        );
 
-      // niece
-      state.niece1 = setFigures(
-        isOdd(state.mother1.firstLine + state.mother2.firstLine),
-        isOdd(state.mother1.secondLine + state.mother2.secondLine),
-        isOdd(state.mother1.thirdLine + state.mother2.thirdLine),
-        isOdd(state.mother1.fourthLine + state.mother2.fourthLine)
-      );
+        // niece
+        state.niece1 = setFigures(
+          isOdd(state.mother1.firstLine + state.mother2.firstLine),
+          isOdd(state.mother1.secondLine + state.mother2.secondLine),
+          isOdd(state.mother1.thirdLine + state.mother2.thirdLine),
+          isOdd(state.mother1.fourthLine + state.mother2.fourthLine)
+        );
 
-      state.niece2 = setFigures(
-        isOdd(state.mother3.firstLine + state.mother4.firstLine),
-        isOdd(state.mother3.secondLine + state.mother4.secondLine),
-        isOdd(state.mother3.thirdLine + state.mother4.thirdLine),
-        isOdd(state.mother3.fourthLine + state.mother4.fourthLine)
-      );
+        state.niece2 = setFigures(
+          isOdd(state.mother3.firstLine + state.mother4.firstLine),
+          isOdd(state.mother3.secondLine + state.mother4.secondLine),
+          isOdd(state.mother3.thirdLine + state.mother4.thirdLine),
+          isOdd(state.mother3.fourthLine + state.mother4.fourthLine)
+        );
 
-      state.niece3 = setFigures(
-        isOdd(state.daughter1.firstLine + state.daughter2.firstLine),
-        isOdd(state.daughter1.secondLine + state.daughter2.secondLine),
-        isOdd(state.daughter1.thirdLine + state.daughter2.thirdLine),
-        isOdd(state.daughter1.fourthLine + state.daughter2.fourthLine)
-      );
+        state.niece3 = setFigures(
+          isOdd(state.daughter1.firstLine + state.daughter2.firstLine),
+          isOdd(state.daughter1.secondLine + state.daughter2.secondLine),
+          isOdd(state.daughter1.thirdLine + state.daughter2.thirdLine),
+          isOdd(state.daughter1.fourthLine + state.daughter2.fourthLine)
+        );
 
-      state.niece4 = setFigures(
-        isOdd(state.daughter3.firstLine + state.daughter4.firstLine),
-        isOdd(state.daughter3.secondLine + state.daughter4.secondLine),
-        isOdd(state.daughter3.thirdLine + state.daughter4.thirdLine),
-        isOdd(state.daughter3.fourthLine + state.daughter4.fourthLine)
-      );
+        state.niece4 = setFigures(
+          isOdd(state.daughter3.firstLine + state.daughter4.firstLine),
+          isOdd(state.daughter3.secondLine + state.daughter4.secondLine),
+          isOdd(state.daughter3.thirdLine + state.daughter4.thirdLine),
+          isOdd(state.daughter3.fourthLine + state.daughter4.fourthLine)
+        );
 
-      // rightWitness, lefttWitness and judge
-      state.rightWitness = setFigures(
-        isOdd(state.niece1.firstLine + state.niece2.firstLine),
-        isOdd(state.niece1.secondLine + state.niece2.secondLine),
-        isOdd(state.niece1.thirdLine + state.niece2.thirdLine),
-        isOdd(state.niece1.fourthLine + state.niece2.fourthLine)
-      );
+        // rightWitness, lefttWitness and judge
+        state.rightWitness = setFigures(
+          isOdd(state.niece1.firstLine + state.niece2.firstLine),
+          isOdd(state.niece1.secondLine + state.niece2.secondLine),
+          isOdd(state.niece1.thirdLine + state.niece2.thirdLine),
+          isOdd(state.niece1.fourthLine + state.niece2.fourthLine)
+        );
 
-      state.leftWitness = setFigures(
-        isOdd(state.niece3.firstLine + state.niece4.firstLine),
-        isOdd(state.niece3.secondLine + state.niece4.secondLine),
-        isOdd(state.niece3.thirdLine + state.niece4.thirdLine),
-        isOdd(state.niece3.fourthLine + state.niece4.fourthLine)
-      );
+        state.leftWitness = setFigures(
+          isOdd(state.niece3.firstLine + state.niece4.firstLine),
+          isOdd(state.niece3.secondLine + state.niece4.secondLine),
+          isOdd(state.niece3.thirdLine + state.niece4.thirdLine),
+          isOdd(state.niece3.fourthLine + state.niece4.fourthLine)
+        );
 
-      state.judge = setFigures(
-        isOdd(state.rightWitness.firstLine + state.leftWitness.firstLine),
-        isOdd(state.rightWitness.secondLine + state.leftWitness.secondLine),
-        isOdd(state.rightWitness.thirdLine + state.leftWitness.thirdLine),
-        isOdd(state.rightWitness.fourthLine + state.leftWitness.fourthLine)
-      );
+        state.judge = setFigures(
+          isOdd(state.rightWitness.firstLine + state.leftWitness.firstLine),
+          isOdd(state.rightWitness.secondLine + state.leftWitness.secondLine),
+          isOdd(state.rightWitness.thirdLine + state.leftWitness.thirdLine),
+          isOdd(state.rightWitness.fourthLine + state.leftWitness.fourthLine)
+        );
 
-      // conciliator
-      state.conciliator = setFigures(
-        isOdd(
-          state.judge.firstLine +
-            findLineValue(
-              state[findFigureByHouse(state.houseOfQuestion.num)],
-              'firstLine'
-            )
-        ),
-        isOdd(
-          state.judge.secondLine +
-            findLineValue(
-              state[findFigureByHouse(state.houseOfQuestion.num)],
-              'secondLine'
-            )
-        ),
-        isOdd(
-          state.judge.thirdLine +
-            findLineValue(
-              state[findFigureByHouse(state.houseOfQuestion.num)],
-              'thirdLine'
-            )
-        ),
-        isOdd(
-          state.judge.fourthLine +
-            findLineValue(
-              state[findFigureByHouse(state.houseOfQuestion.num)],
-              'fourthLine'
-            )
-        )
-      );
+        // conciliator
+        state.conciliator = setFigures(
+          isOdd(
+            state.judge.firstLine +
+              findLineValue(
+                state[findFigureByHouse(state.houseOfQuestion.num)],
+                'firstLine'
+              )
+          ),
+          isOdd(
+            state.judge.secondLine +
+              findLineValue(
+                state[findFigureByHouse(state.houseOfQuestion.num)],
+                'secondLine'
+              )
+          ),
+          isOdd(
+            state.judge.thirdLine +
+              findLineValue(
+                state[findFigureByHouse(state.houseOfQuestion.num)],
+                'thirdLine'
+              )
+          ),
+          isOdd(
+            state.judge.fourthLine +
+              findLineValue(
+                state[findFigureByHouse(state.houseOfQuestion.num)],
+                'fourthLine'
+              )
+          )
+        );
 
-      // part of fortune
-      state.partOfFortune.house = setPartOfFortune(current(state));
+        // part of fortune
+        state.partOfFortune.house = setPartOfFortune(current(state));
+      }
     },
   },
 });
