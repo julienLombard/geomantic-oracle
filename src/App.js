8cfb0e271826540fc2652c56d5dfe38010ea96d2
julienLombard <julienlombard.fr@gmail.com>
2022-05-13 17:10:21 +0200

    adding ThemeReader.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GeomanticTheme } from './features/geomanticTheme/GeomanticTheme';
 import './App.css';
 import { mountTheme } from './features/geomanticTheme/geomanticThemeSlice';
 import { useDispatch } from 'react-redux';
+import { ThemeReader } from './features/themeReader/ThemeReader';
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <GeomanticTheme />
+        <ThemeReader />
       </header>
     </div>
   );
