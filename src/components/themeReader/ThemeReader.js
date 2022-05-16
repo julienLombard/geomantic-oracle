21a6abdc704766aa096718827623448da0965f37
julienLombard <julienlombard.fr@gmail.com>
2022-05-17 01:11:43 +0200

    fixing display problem  in themeReader.js

--- src/components/themeReader/ThemeReader.js
+++ src/components/themeReader/ThemeReader.js
@@ -25,35 +25,35 @@ export const ThemeReader = (props) => {
               <th scope="row">Maison 2</th>
               <td>
                 {data[figures.daughter2.name] &&
-                  data[figures.daughter2.name].maison_1}
+                  data[figures.daughter2.name].maison_2}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 3</th>
               <td>
                 {data[figures.niece2.name] &&
-                  data[figures.niece2.name].maison_1}
+                  data[figures.niece2.name].maison_3}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 4</th>
               <td>
                 {data[figures.mother3.name] &&
-                  data[figures.mother3.name].maison_1}
+                  data[figures.mother3.name].maison_4}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 5</th>
               <td>
                 {data[figures.daughter3.name] &&
-                  data[figures.daughter3.name].maison_1}
+                  data[figures.daughter3.name].maison_5}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 6</th>
               <td>
                 {data[figures.niece3.name] &&
-                  data[figures.niece3.name].maison_1}
+                  data[figures.niece3.name].maison_6}
               </td>
             </tr>
           </tbody>
@@ -65,42 +65,42 @@ export const ThemeReader = (props) => {
               <th scope="row">Maison 7</th>
               <td>
                 {data[figures.mother4.name] &&
-                  data[figures.mother4.name].maison_1}
+                  data[figures.mother4.name].maison_7}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 8</th>
               <td>
                 {data[figures.daughter4.name] &&
-                  data[figures.daughter4.name].maison_1}
+                  data[figures.daughter4.name].maison_8}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 9</th>
               <td>
                 {data[figures.niece4.name] &&
-                  data[figures.niece4.name].maison_1}
+                  data[figures.niece4.name].maison_9}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 10</th>
               <td>
                 {data[figures.mother1.name] &&
-                  data[figures.mother1.name].maison_1}
+                  data[figures.mother1.name].maison_10}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 11</th>
               <td>
                 {data[figures.daughter1.name] &&
-                  data[figures.daughter1.name].maison_1}
+                  data[figures.daughter1.name].maison_11}
               </td>
             </tr>
             <tr>
               <th scope="row">Maison 12</th>
               <td>
                 {data[figures.niece1.name] &&
-                  data[figures.niece1.name].maison_1}
+                  data[figures.niece1.name].maison_12}
               </td>
             </tr>
           </tbody>
@@ -111,20 +111,20 @@ export const ThemeReader = (props) => {
               <th scope="row">Témoin droit</th>
               <td>
                 {data[figures.rightWitness.name] &&
-                  data[figures.rightWitness.name].maison_1}
+                  data[figures.rightWitness.name].general}
               </td>
             </tr>
             <tr>
               <th scope="row">Témoin Gauche</th>
               <td>
                 {data[figures.leftWitness.name] &&
-                  data[figures.leftWitness.name].maison_1}
+                  data[figures.leftWitness.name].general}
               </td>
             </tr>
             <tr>
               <th scope="row">Juge</th>
               <td>
-                {data[figures.judge.name] && data[figures.judge.name].maison_1}
+                {data[figures.judge.name] && data[figures.judge.name].general}
               </td>
             </tr>
             <tr>
@@ -137,8 +137,8 @@ export const ThemeReader = (props) => {
             <tr>
               <th scope="row">Part de fortune</th>
               <td>
-                {data[figures.partOfFortune.name] &&
-                  data[figures.partOfFortune.name].maison_1}
+                {figures.partOfFortune.house &&
+                  'Maison ' + figures.partOfFortune.house}
               </td>
             </tr>
           </tbody>
