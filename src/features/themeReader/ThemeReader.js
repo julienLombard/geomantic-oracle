d96abbf93e23952e687f5601916cd9878bf42241
julienLombard <julienlombard.fr@gmail.com>
2022-05-16 17:02:21 +0200

    adding themeReader.css

--- src/features/themeReader/ThemeReader.js
+++ src/features/themeReader/ThemeReader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './themeReader.css';
 
 export const ThemeReader = () => {
   return (
@@ -7,172 +8,174 @@ export const ThemeReader = () => {
         <button type="submit">Intepretation</button>
       </form>
 
-      <table>
-        <tbody>
-          <tr>
-            <th scope="row">Maison 1</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              hic blanditiis quisquam error est libero expedita, quas natus
-              corrupti saepe magnam ipsa quam, tempore veritatis. Consectetur
-              odit minus architecto itaque.
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 2</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 3</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 4</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 5</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 6</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="interpretation-table">
+        <table>
+          <tbody>
+            <tr>
+              <th scope="row">Maison 1</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                hic blanditiis quisquam error est libero expedita, quas natus
+                corrupti saepe magnam ipsa quam, tempore veritatis. Consectetur
+                odit minus architecto itaque.
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 2</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 3</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 4</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 5</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 6</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-      <table>
-        <tbody>
-          <tr>
-            <th scope="row">Maison 7</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              hic blanditiis quisquam error est libero expedita, quas natus
-              corrupti saepe magnam ipsa quam, tempore veritatis. Consectetur
-              odit minus architecto itaque.
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 8</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 9</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 10</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 11</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Maison 12</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <tbody>
-          <tr>
-            <th scope="row">Témoin droit</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              hic blanditiis quisquam error est libero expedita, quas natus
-              corrupti saepe magnam ipsa quam, tempore veritatis. Consectetur
-              odit minus architecto itaque.
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Témoin Gauche</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Juge</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Conciliator</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">Part de fortune</th>
-            <td>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-              necessitatibus assumenda consequuntur illo quas excepturi dicta
-              totam, exercitationem perspiciatis recusandae, cumque expedita
-              quibusdam id quisquam iste explicabo hic sit. Qui?
-            </td>
-          </tr>
-        </tbody>
-      </table>
+        <table>
+          <tbody>
+            <tr>
+              <th scope="row">Maison 7</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                hic blanditiis quisquam error est libero expedita, quas natus
+                corrupti saepe magnam ipsa quam, tempore veritatis. Consectetur
+                odit minus architecto itaque.
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 8</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 9</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 10</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 11</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Maison 12</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table>
+          <tbody>
+            <tr>
+              <th scope="row">Témoin droit</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                hic blanditiis quisquam error est libero expedita, quas natus
+                corrupti saepe magnam ipsa quam, tempore veritatis. Consectetur
+                odit minus architecto itaque.
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Témoin Gauche</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Juge</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Conciliator</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">Part de fortune</th>
+              <td>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                necessitatibus assumenda consequuntur illo quas excepturi dicta
+                totam, exercitationem perspiciatis recusandae, cumque expedita
+                quibusdam id quisquam iste explicabo hic sit. Qui?
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
